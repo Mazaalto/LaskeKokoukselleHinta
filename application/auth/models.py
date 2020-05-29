c90385dec03f116ed1bdc6c6ce71089895e2bc2a
Matias Aalto-Setälä <matias.aaltosetala@gmail.com>
2020-05-29 10:40:46 +0300

    Ulkoasun viilaamista

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,26 +1,23 @@
 from application import db
+from application.models import Base
 
-class User(db.Model):
+class User(Base):
 # koska sana user on varattu avainsana myöh. käytettävässä PostgreSQLssä  niin käytetään
 # tietokantataulussa nimeä account
     __tablename__ = "account"
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     username = db.Column(db.String(144), nullable=False)
     password = db.Column(db.String(144), nullable=False)
 
     tasks = db.relationship("Task", backref='account', lazy=True)
-  
+
     def __init__(self, name):
         self.name = name
-        
+
     def get_id(self):
         return self.id
-  
+
     def is_active(self):
         return True
 
